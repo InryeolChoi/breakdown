a1f5fdca3152c94744cd3952070651f95ac2c20f
InryeolChoi <dlsfuf0316@gmail.com>
2026-09-17 00:51:33 +0900

    feat: map Room entity and validate database schema

--- src/main/java/com/breakground/room/Room.java
+++ src/main/java/com/breakground/room/Room.java
@@ -1,12 +1,20 @@
 package com.breakground.room;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 import java.time.LocalTime;
 
 @Getter
+@Entity
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "name", length = 10, nullable = false)
     private String name;
+    @Column(name = "open_at", nullable = false)
     private LocalTime openAt;
+    @Column(name = "close_at", nullable = false)
     private LocalTime closeAt;
 }
